ab040470cae4999d0c573de1d03c57491a1d70c7
Kevin <kevin.schorkops@arexo.be>
2016-08-30 14:18:22 +0200

    Episode 55

--- Schorke/src/com/schorkegame/schorke/level/SpawnLevel.java
+++ Schorke/src/com/schorkegame/schorke/level/SpawnLevel.java
@@ -21,6 +21,7 @@ public class SpawnLevel extends Level {
 			int w = image.getWidth();
 			int h = image.getHeight();
 			tiles = new Tile[w * h];
+			levelPixels = new int[w * h];
 			image.getRGB(0, 0, w, h, levelPixels, 0, w);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -29,13 +30,13 @@ public class SpawnLevel extends Level {
 
 	protected void generateLevel() {
 		for(int i = 0; i < levelPixels.length; i++) {
-			if(levelPixels[i] == 0xFF00) {
+			if(levelPixels[i] == 0xff00ff00) {
 				tiles[i] = Tile.grass;
 			}
-			if(levelPixels[i] == 0xFFFF00) {
+			if(levelPixels[i] == 0xffffff00) {
 				tiles[i] = Tile.flower;
 			}
-			if(levelPixels[i] == 0x7F7F00) {
+			if(levelPixels[i] == 0xff7f7f00) {
 				tiles[i] = Tile.rock;
 			}
 		}
